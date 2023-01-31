0649c9b1aed881cc471f48766ab074e62a992aff
clazkani <chadilazjobb@hotmail.com>
2023-01-31 14:11:43 +0100

    accessing UAC_rover cloned repo

--- tests/test_calc_eff.py
+++ tests/test_calc_eff.py
@@ -1,6 +1,6 @@
 # from loggingFile import logg
 import pytest
-from power_control.SolarPanels import SolarPanels
+from ..power_control.SolarPanels import SolarPanels
 
 
 @pytest.mark.parametrize("temp, eff", [
