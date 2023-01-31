52bea203f721c1538d046ab2f44fa8780fea294e
clazkani <chadilazjobb@hotmail.com>
2023-01-31 16:00:50 +0100

    running all tests

--- tests_main.py
+++ tests_main.py
@@ -1,5 +1,22 @@
+import os
 import pytest
 
 
+def main():
+    """Something"""
+
+    arr = os.listdir("tests")
+    out = []
+    for file_test in arr:
+        if "test" in file_test[0:4] and ".py" in file_test:
+            out.append(f"tests/{file_test}")
+
+    print(out)
+
+    return out
+
+
 if __name__ == "__main__":
-    pytest.main(["-v", "tests/*"])
+    files = main()
+    for file in files:
+        pytest.main(["-v", file])
