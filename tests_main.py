f7268bc2c64366f0a8ad96a6d91c856dee7bec0a
clazkani <chadilazjobb@hotmail.com>
2023-02-01 10:22:28 +0100

    fixed main

--- tests_main.py
+++ tests_main.py
@@ -1,22 +1,7 @@
-import os
 import pytest
 
 
-def main():
-    """Something"""
-
-    arr = os.listdir("tests")
-    out = []
-    for file_test in arr:
-        if "test" in file_test[0:4] and ".py" in file_test:
-            out.append(f"tests/{file_test}")
-
-    print(out)
-
-    return out
 
 
 if __name__ == "__main__":
-    #files = main()
-    #for file in files:
     pytest.main(["-v", "tests/"])
